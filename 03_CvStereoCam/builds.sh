c5f7918335034bbeceb84b1512be0c8c7dfb1b5d
Andrey E. Yaromenok <ayaromenok@gmail.com>
2018-10-30 12:22:25 +0200

    #update with tripleCam

--- 03_CvStereoCam/builds.sh
+++ 03_CvStereoCam/builds.sh
@@ -1,2 +1,3 @@
 # -lstdc++ added fro pri3 build
 gcc stereoCam.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui -lopencv_videoio -lstdc++ -I /usr/local/include/opencv4/ -o stereoCam
+gcc tripleCam.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui -lopencv_videoio -lstdc++ -I /usr/local/include/opencv4/ -o tripleCam

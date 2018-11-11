d5ef39cddc783d1672e7ff5a409c5bb7bc163f92
Andrey E. Yaromenok <ayaromenok@gmail.com>
2018-11-11 09:59:35 +0200

    make one test for CPU/CL and 512, 1k, 2k 4k images

--- 04_CvClCuda/builds.sh
+++ 04_CvClCuda/builds.sh
@@ -1,3 +1,3 @@
 # -lstdc++ added fro pri3 build
 gcc testCPU.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui -lopencv_videoio -lstdc++ -I /usr/local/include/opencv4/ -o testCPU
-gcc testCL.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui -lopencv_videoio -lstdc++ -I /usr/local/include/opencv4/ -o testCL
+#gcc testCUDA.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui -lopencv_videoio -lstdc++ -lcuda -I /usr/local/include/opencv4/ -o testCUDA
--- 04_CvClCuda/testCPU.cpp
+++ 04_CvClCuda/testCPU.cpp
@@ -1,22 +1,54 @@
 #include <iostream>
 #include "opencv2/opencv.hpp"
 using namespace cv;
- 
-int main(int argc, char** argv)
-{
-    Mat img, gray;
-    int64 e1, e2, time;
-    imread("lena4k.jpg", IMREAD_COLOR).copyTo(img);
+using namespace std;
 
-    e1= cv::getTickCount(); 
+double testImage(const string &inImage){
+    Mat img, gray;
+    cv::TickMeter pc;
     
+    imread(inImage, IMREAD_COLOR).copyTo(img);
+    
+    pc.start();
     cvtColor(img, gray, COLOR_BGR2GRAY);
     GaussianBlur(gray, gray,Size(7, 7), 1.5);
     Canny(gray, gray, 0, 50);
+    pc.stop();
     
-    e2 = cv::getTickCount();
-    time = (1000*(e2-e1))/cv::getTickFrequency();
-    std::cout << "time: " << time << std::endl;
+    return pc.getTimeMilli();
+}
 
+double testImageCL(const string &inImage){
+    UMat img, gray; // CL via Transparent API
+    cv::TickMeter pc;
+    
+    imread(inImage, IMREAD_COLOR).copyTo(img);
+    
+    pc.start();
+    cvtColor(img, gray, COLOR_BGR2GRAY);
+    GaussianBlur(gray, gray,Size(7, 7), 1.5);
+    Canny(gray, gray, 0, 50);
+    pc.stop();
+    
+    return pc.getTimeMilli();
+}
+ 
+int main(int argc, char** argv)
+{
+    string strLena512("lena512.jpg");
+    string strLena1k("lena1k.jpg");
+    string strLena2k("lena2k.jpg");
+    string strLena4k("lena4k.jpg");
+    
+    cout << "Test CPU:" << endl;
+    cout << "time: " << testImage(strLena512) << " msec" << endl;   
+    cout << "time: " << testImage(strLena1k) << " msec" << endl;   
+    cout << "time: " << testImage(strLena2k) << " msec" << endl;   
+    cout << "time: " << testImage(strLena4k) << " msec" << endl;   
+    cout << "Test GPU/CL:" << endl;
+    cout << "time: " << testImageCL(strLena512) << " msec" << endl;   
+    cout << "time: " << testImageCL(strLena1k) << " msec" << endl;   
+    cout << "time: " << testImageCL(strLena2k) << " msec" << endl;   
+    cout << "time: " << testImageCL(strLena4k) << " msec" << endl;   
     return 0;
 }

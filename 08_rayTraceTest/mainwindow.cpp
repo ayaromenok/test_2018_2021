aeb4a14bd03756c13cad74b0f3840c6c891e5bba
Andrey Yaromenok <ayaromenok@gmail.com>
2019-01-21 10:43:51 +0200

    #8 missed cahnegs from previous commit

--- 08_rayTraceTest/mainwindow.cpp
+++ 08_rayTraceTest/mainwindow.cpp
@@ -1,11 +1,42 @@
 #include "mainwindow.h"
+#include<QtWidgets>
 
 MainWindow::MainWindow(QWidget *parent)
-: QMainWindow(parent)
+: QWidget(parent)
 {
+    setUI();
 }
 
 MainWindow::~MainWindow()
 {
 
+}
+
+void
+MainWindow::setUI()
+{
+    _loutHBox = new QHBoxLayout(this);
+    _loutImg = new QVBoxLayout();
+    _loutCtrl = new QVBoxLayout();
+    _gboxImg = new QGroupBox("image");
+    _gboxImg->setMinimumSize(240,240);
+    _gboxCtrl = new QGroupBox("control");
+    _gboxCtrl->setMinimumSize(120,240);
+    _lbImg = new QLabel("image");
+    _pxmpImg = new QPixmap(128,96);
+    _lbImg->setPixmap(*_pxmpImg);
+    _lbCtrl = new QLabel("control");
+    _lbCtrlNext = new QLabel("control next");
+
+    _loutImg->addWidget(_lbImg);
+    _loutCtrl->addWidget(_lbCtrl);
+    _loutCtrl->addWidget(_lbCtrlNext);
+
+    _gboxImg->setLayout(_loutImg);
+    _gboxCtrl->setLayout(_loutCtrl);
+
+    _loutHBox->addWidget(_gboxImg);
+    _loutHBox->addWidget(_gboxCtrl);
+
+    this->setLayout(_loutHBox);
 }
--- 08_rayTraceTest/mainwindow.h
+++ 08_rayTraceTest/mainwindow.h
@@ -1,15 +1,32 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <QWidget>
+class QHBoxLayout;
+class QVBoxLayout;
+class QGroupBox;
+class QLabel;
+class QPixmap;
 
-class MainWindow : public QMainWindow
+class MainWindow : public QWidget
 {
-Q_OBJECT
+    Q_OBJECT
 
 public:
-MainWindow(QWidget *parent = 0);
-~MainWindow();
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    void            setUI();
+    QHBoxLayout     *_loutHBox;
+    QGroupBox       *_gboxImg;
+    QGroupBox       *_gboxCtrl;
+    QVBoxLayout     *_loutImg;
+    QVBoxLayout     *_loutCtrl;
+    QLabel          *_lbImg;
+    QLabel          *_lbCtrl;
+    QLabel          *_lbCtrlNext;
+    QPixmap         *_pxmpImg;
 };
 
 #endif // MAINWINDOW_H

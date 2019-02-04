89d21201c53f2c360aa1d573c91c708ee1ce29be
Andrey Yaromenok <ayaromenok@gmail.com>
2019-02-04 19:28:27 +0200

    video2files for rk3399 added

---
+++ 09_CvGStreamerCam/video2fileRk3399.sh
@@ -0,0 +1,1 @@
+gst-launch-1.0 rkisp device=/dev/video0 sensor-id=1 io-mode=4 path-iqf=/etc/cam_iq/rk-ov4689.xml ! video/x-raw,format=NV12,width=1280,height=720,framerate=30/1 ! videoconvert ! x264enc ! avimux ! filesink location=testCam1.avi

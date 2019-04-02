55a29c0487453406b80ccb1640f315b42236266f
Andrey Yaromenok/RPi3 <ayaromenok@gmail.com>
2019-04-02 07:48:02 -0400

    added SPI/WiringPi stub app

---
+++ 11_AMG8833_i2c_Rpi/amg8833_i2c.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <errno.h>
+#include <wiringPiI2C.h>
+
+using namespace std;
+
+int main()
+{
+   int fd, result;
+
+    // 0x69 - is a AMG8833 number. 
+    // looks like can be 0x68 for original adaruit sensor
+    // can be checked with `gpio i2cdetect` command
+   fd = wiringPiI2CSetup(0x69);
+
+   cout << "Init result: "<< fd << endl;
+
+   for(int i = 0; i < 0x0000000f; i++)
+   {
+
+      result = wiringPiI2CWriteReg8(fd, 0x40, (i & 0xf) );
+
+      if(result == -1)
+      {
+         cout << "Error.  Errno is: " << errno << endl;
+      }
+   }
+}
+
---
+++ 11_AMG8833_i2c_Rpi/build.sh
@@ -0,0 +1,1 @@
+g++ amg8833_i2c.cpp -lwiringPi -o testAmg8833_i2c

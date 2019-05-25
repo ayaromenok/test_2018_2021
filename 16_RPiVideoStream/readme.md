37ed312f1bfdcb28485bf5659dc31e8d81e5bc4c
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-05-25 14:39:57 +0300

    uupdate to RpiCam/MJpeg with Eth0 instead of wifi

--- 16_RPiVideoStream/readme.md
+++ 16_RPiVideoStream/readme.md
@@ -6,6 +6,11 @@ Send videostream from Rpi(stereo in particular) to another devboards with suitab
  - runRaspiVidStereo_1296x486.sh - ok
  - runMJpegStreamer - ok. no stereo
 	[requre mjpg-streamer](https://github.com/jacksonliam/mjpg-streamer)
+	can be lag, which rlated to network speed. 
+	320x240 - 90fps on eth0
+	640x480 - 30fps on eth0, less on wifi
+	1296x972 - 15fps on eth0, around 10 on wifi
+	2592x1944 - around 4-5fps on eth0
 	
 ### Host
 
--- 16_RPiVideoStream/rpi/runMJpegStreamer.sh
+++ 16_RPiVideoStream/rpi/runMJpegStreamer.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 # no 3D support
-# fps bigger than 15 give a additional lag
-mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -fps 15"
+# lag related to network speed
+# max 90 fps due to native 640x480x90 max
+# 
+
+#mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -width 160 -height 120 -fps 90"
+mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -width 320 -height 240 -fps 90"
+#mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -width 640 -height 480 -fps 30"
+#mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -width 1296 -height 972 -fps 15"
+#mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -width 2592 -height 1944 -fps 5"

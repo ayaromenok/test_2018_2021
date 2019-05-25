358decccaa2dca1ac946d9993b3db45f093c1220
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-05-25 05:59:50 +0300

    DISPLAY exported for rpi\stream

--- 16_RPiVideoStream/host/_bnrCvMJpegStream.sh
+++ 16_RPiVideoStream/host/_bnrCvMJpegStream.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # -lstdc++ for RPi3 build
+export DISPLAY=:0.0
 g++ cvMJpegStream.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui -lopencv_videoio -lstdc++ -I /usr/local/include/opencv4/ -L/usr/local/lib \
 -o cvMJpegStream
 ./cvMJpegStream

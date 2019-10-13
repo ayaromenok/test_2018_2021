43bf0983ce852e64d34075e08e24b32dbad3ba5b
Andrey Yaromenok/TegraX1 <ayaromenok@gmail.com>
2019-10-13 11:53:23 +0200

    added build script for Jetson Nano

---
+++ 17_cvCamJetsonNano/_build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# -lstdc++ for RPi3 build
+export DISPLAY=:0.0
+g++ cvCamJetsonNano.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui -lopencv_videoio -lstdc++ -I /usr/local/include/opencv4/ -L/usr/local/lib \
+-o cvCamJetsonNano
+./cvCamJetsonNano

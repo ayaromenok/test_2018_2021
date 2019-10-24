72868d54aab03e21772aaeb8406d68383588c1e2
Andrey Yaromenok/TegraX1 <ayaromenok@gmail.com>
2019-10-24 18:40:45 +0200

    #25 2 fixes: name of required ARm extentions and constructor initialization of CSinExt

--- 25_ComputeGL/glwindow.cpp
+++ 25_ComputeGL/glwindow.cpp
@@ -10,7 +10,7 @@
 #include <QOpenGLVertexArrayObject>
 
 GLWindow::GLWindow()
-    :m_isExtHasCompute(true),
+    :m_isExtHasCompute(false),
      m_isAnimationForward(true),
      m_blurCur(0.0f),
      m_blurMin(0.0f),
@@ -43,7 +43,7 @@ GLWindow::versionedShaderCode(const char *src)
     if (QOpenGLContext::currentContext()->isOpenGLES()){
         versionedSrc.append(QByteArrayLiteral("#version 310 es\n"));
     } else if (m_isExtHasCompute) {
-        versionedSrc.append(QByteArrayLiteral("#version 420 core\n#extension ARB_compute_shader: enable\n"));
+        versionedSrc.append(QByteArrayLiteral("#version 420 core\n#extension GL_ARB_compute_shader: enable\n"));
     } else {
         versionedSrc.append(QByteArrayLiteral("#version 430 core\n"));
     }

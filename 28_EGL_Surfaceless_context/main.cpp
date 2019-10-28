e156100e675577df2c9039ae4dc2e8212f4aea70
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-10-28 15:39:14 +0100

    #28 make build with GLESv2

--- 28_EGL_Surfaceless_context/main.cpp
+++ 28_EGL_Surfaceless_context/main.cpp
@@ -1,8 +1,19 @@
 #include <QCoreApplication>
+#include <QDebug>
+
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <GLES3/gl31.h>
+#include <fcntl.h>
+#include <gbm.h>
+
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    int32_t fd = open ("/dev/dri/renderD128", O_RDWR);
+    qDebug() << "fd" << fd;
 
     return a.exec();
 }

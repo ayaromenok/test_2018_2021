defad317de241c024bc20588b3ffece9a2c0d679
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-11-01 18:17:31 +0100

    #29 spell fix to data script and gitignore for it

--- 29_Keras_VGG16_etc/getData.sh
+++ 29_Keras_VGG16_etc/getData.sh
@@ -1,4 +1,4 @@
 wget https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip
 unzip  cats_and_dogs_filtered.zip
-ln -s cats_and_dogs_filtere data
+ln -s cats_and_dogs_filtered data
 rm -f cats_and_dogs_filtered.zip

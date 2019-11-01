36939b9c2a56f14be56c83482b2425adcb68a3f4
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-11-01 08:25:57 +0100

    #29 Keras implementation of VGG16 - default

---
+++ 29_Keras_VGG16_etc/testImage_224.py
@@ -0,0 +1,23 @@
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+
+imgShow=mpimg.imread("image.jpg")
+imgplot = plt.imshow(imgShow)
+plt.show()
+
+img = image.load_img("image.jpg",target_size=(224,224))
+img = np.asarray(img)
+plt.imshow(img)
+img = np.expand_dims(img, axis=0)
+
+from keras.models import load_model
+saved_model = load_model("vgg16_1.h5")
+
+output = saved_model.predict(img)
+if output[0][0] > output[0][1]:
+    print("cat")
+else:
+    print('dog')

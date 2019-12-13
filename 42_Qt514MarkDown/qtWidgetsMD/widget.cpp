71828868d16f5f061861c83d9fd1002e31a24aa3
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-12-13 13:22:19 +0100

    #4 added working MD example on create

--- 42_Qt514MarkDown/qtWidgetsMD/widget.cpp
+++ 42_Qt514MarkDown/qtWidgetsMD/widget.cpp
@@ -6,6 +6,7 @@
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
+    setGeometry(100,100,640,400);
     QVBoxLayout *lt = new QVBoxLayout(this);
     QTextDocument *td = new QTextDocument("text");
     QTextEdit *te = new QTextEdit();
@@ -13,11 +14,16 @@ Widget::Widget(QWidget *parent)
     lt->addWidget(te);
     setLayout(lt);
 
+    td->setMarkdown("# MD/GitHub\n## Level 2\n - [MD link](http://www.example.com)\n"
+                    " - [local link](#llink)\n\n### Level 3\n Table\n\n"
+                    "| Title #1 | Title #2 |\n|---|---|\n|value #1| value #2|",
+                    QTextDocument::MarkdownDialectGitHub);
 
-    QTextCursor *tc = new QTextCursor(td);
-    tc->setPosition(4);
-    tc->insertText("\nadditional text");
-    tc->insertText("\n[MD link](http://www.example.com)");
+//    QTextCursor *tc = new QTextCursor(td);
+//    tc->setPosition(4);
+//    tc->insertText("\nadditional text");
+//    tc->insertText("\n[MD link](http://www.example.com)");
+
 
 }
 

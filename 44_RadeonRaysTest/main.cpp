8070577b18e26e1ae23788fadc1c9b9d7c17a473
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-12-17 09:24:23 +0100

    #44 stub for radeonRays exampls

---
+++ 44_RadeonRaysTest/main.cpp
@@ -0,0 +1,9 @@
+//Copyrigth (C) 2019 Andrey Yaromenok
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}
---
+++ 44_RadeonRaysTest/readme.md
@@ -0,0 +1,1 @@
+radeon ray should be build with embedded OpenCL kernels (RR_EMBED_KERNELS)

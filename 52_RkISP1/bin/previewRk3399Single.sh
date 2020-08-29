00e6a264f76a2216fe6fda002c12ba948daeab5a
Andrey Yaromneok <ayaromenok@gmail.com>
2020-08-29 05:52:01 +0000

    #52 added preview for rk3399/single/dual and readme with *sink description

---
+++ 52_RkISP1/bin/previewRk3399Single.sh
@@ -0,0 +1,5 @@
+#rkimagesink required io-mode=4 and work correctly only with sinle camera(but faster)
+#glimagesink - for dual cam(but with delay)
+export DISPLAY=:0.0
+
+gst-launch-1.0 rkisp device=/dev/video0 io-mode=4 path-iqf=../cam_iq/ov4689.xml ! video/x-raw,format=NV12,width=640,height=360,framerate=30/1 ! rkximagesink
---
+++ 52_RkISP1/readme.md
@@ -0,0 +1,6 @@
+
+
+### Image sinks
+ - rkximagesink - X11, fast. require io-mode=4
+ - glimagesink - GL, some delay, works with dual-cam
+ - multifilesink - save to file. since AWB required some time to work correctly, tupically need 3-4 frames to stabilize AWB. num-buffers - # of files

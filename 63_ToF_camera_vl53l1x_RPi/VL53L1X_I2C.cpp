ff39b2c5f61872d202cdc2181449c7bb3bc44c7a
Andrey Yaromenok <ayaromenok@gmail.com>
2021-02-05 12:30:20 +0200

    #63 i2c stub added

---
+++ 63_ToF_camera_vl53l1x_RPi/VL53L1X_I2C.cpp
@@ -0,0 +1,65 @@
+/*
+Hardware connections:
+
+This file interfaces with the SparkFun MCP4725 breakout board:
+https://www.sparkfun.com/products/8736
+
+The board was connected as follows:
+(Raspberry Pi)(MCP4725)
+GND  -> GND
+3.3V -> Vcc
+SCL  -> SCL
+SDA  -> SDA
+
+An oscilloscope probe was connected to the analog output pin of the MCP4725.
+
+To build this file, I use the command:
+>  g++ i2ctest.cpp -lwiringPi
+
+Then to run it, first the I2C kernel module needs to be loaded.  This can be 
+done using the GPIO utility.
+> gpio load i2c 400
+> ./a.out
+
+******************************************************************************/
+
+#include <iostream>
+#include <errno.h>
+#include <wiringPiI2C.h>
+
+using namespace std;
+
+int main()
+{
+   int fd, result;
+
+   // Initialize the interface by giving it an external device ID.
+   // The MCP4725 defaults to address 0x60.   
+   //
+   // It returns a standard file descriptor.
+   // 
+   fd = wiringPiI2CSetup(0x29);
+
+   cout << "Init result: "<< fd << endl;
+/*
+   for(int i = 0; i < 0x0000ffff; i++)
+   {
+      // I tried using the "fast write" command, but couldn't get it to work.  
+      // It's not entirely obvious what's happening behind the scenes as
+      // regards to endianness or length of data sent.  I think it's only 
+      // sending one byte, when we really need two.
+      //
+      // So instead I'm doing a 16 bit register access.  It appears to 
+      // properly handle the endianness, and the length is specified by the 
+      // call.  The only question was the register address, which is the 
+      // concatenation of the command (010x = write DAC output) 
+      // and power down (x00x = power up) bits.
+      result = wiringPiI2CWriteReg16(fd, 0x40, (i & 0xfff) );
+
+      if(result == -1)
+      {
+         cout << "Error.  Errno is: " << errno << endl;
+      }
+   }
+*/
+}
---
+++ 63_ToF_camera_vl53l1x_RPi/_build.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+g++ VL53L1X_I2C.cpp -l wiringPi -o VL53L1X_I2C
